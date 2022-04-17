8dc42f4578eaebd5bfe34d92add78cc15dace921
Mateusz Bogusz <mateusz.bogusz@wisniowski.pl>
2022-04-17 20:22:11 +0200

    Single quote replaced with double quote where possible

--- torrenter.user.js
+++ torrenter.user.js
@@ -26,7 +26,7 @@
 // @grant          GM_setValue
 // @grant          GM_registerMenuCommand
 // ==/UserScript==
-'use strict';
+"use strict";
 var TorrenterConfigurator = /** @class */ (function () {
     function TorrenterConfigurator(language, changeCallback) {
         var _this = this;
@@ -35,50 +35,50 @@ var TorrenterConfigurator = /** @class */ (function () {
             return;
         }
         var gmConfiguration = {
-            'id': 'mabakay_Torrenter',
-            'title': this.localization.settingsTitle,
-            'fields': {
-                'showEngines': {
-                    'label': this.localization.showBuildInEngines,
-                    'type': 'checkbox',
-                    'default': true
+            "id": "mabakay_Torrenter",
+            "title": this.localization.settingsTitle,
+            "fields": {
+                "showEngines": {
+                    "label": this.localization.showBuildInEngines,
+                    "type": "checkbox",
+                    "default": true
                 },
-                'showUserEngines': {
-                    'label': this.localization.showUserEngines,
-                    'type': 'checkbox',
-                    'default': false
+                "showUserEngines": {
+                    "label": this.localization.showUserEngines,
+                    "type": "checkbox",
+                    "default": false
                 },
-                'showUserEnginesFirst': {
-                    'label': this.localization.showUserEnginesFirst,
-                    'type': 'checkbox',
-                    'default': false
+                "showUserEnginesFirst": {
+                    "label": this.localization.showUserEnginesFirst,
+                    "type": "checkbox",
+                    "default": false
                 },
-                'userEngines': {
-                    'label': this.localization.userEngines,
-                    'type': 'textarea',
-                    'default': '',
+                "userEngines": {
+                    "label": this.localization.userEngines,
+                    "type": "textarea",
+                    "default": "",
                 }
             },
-            'events': {
-                'open': function (document, window, frame) {
-                    var userEnginesFiled = document.getElementById('mabakay_Torrenter_field_userEngines');
-                    userEnginesFiled.setAttribute('cols', '80');
-                    userEnginesFiled.setAttribute('rows', '10');
-                    userEnginesFiled.setAttribute('placeholder', _this.localization.eg + ' https://search-site.com/?title={title}&year={year}&orderby=seeds[&imdbTag={imdb}]');
+            "events": {
+                "open": function (document, window, frame) {
+                    var userEnginesFiled = document.getElementById("mabakay_Torrenter_field_userEngines");
+                    userEnginesFiled.setAttribute("cols", "80");
+                    userEnginesFiled.setAttribute("rows", "10");
+                    userEnginesFiled.setAttribute("placeholder", _this.localization.eg + " https://search-site.com/?title={title}&year={year}&orderby=seeds[&imdbTag={imdb}]");
                     var enginesFieldDescription = document.createElement("div");
-                    enginesFieldDescription.setAttribute('style', 'font-size: 12px;margin: 5px 6px;color: gray;');
+                    enginesFieldDescription.setAttribute("style", "font-size: 12px;margin: 5px 6px;color: gray;");
                     enginesFieldDescription.innerHTML = _this.localization.addEngineDescription;
-                    var enginesLabelField = document.getElementById('mabakay_Torrenter_userEngines_field_label');
+                    var enginesLabelField = document.getElementById("mabakay_Torrenter_userEngines_field_label");
                     enginesLabelField.after(enginesFieldDescription);
-                    var saveButton = document.getElementById('mabakay_Torrenter_saveBtn');
+                    var saveButton = document.getElementById("mabakay_Torrenter_saveBtn");
                     saveButton.textContent = _this.localization.saveButtonCaption;
-                    var closeButton = document.getElementById('mabakay_Torrenter_closeBtn');
+                    var closeButton = document.getElementById("mabakay_Torrenter_closeBtn");
                     closeButton.textContent = _this.localization.closeCaptionButton;
-                    var restToDefaultsLink = document.getElementById('mabakay_Torrenter_resetLink');
+                    var restToDefaultsLink = document.getElementById("mabakay_Torrenter_resetLink");
                     restToDefaultsLink.textContent = _this.localization.resetLinkCaption;
-                    GM_config.frame.setAttribute('style', 'inset: 166px auto auto 326px;border: 1px solid rgb(0, 0, 0);height: 440px;margin: 0px;opacity: 1;overflow: auto;padding: 0px;position: fixed;width: 650px;z-index: 9999;display: block;');
+                    GM_config.frame.setAttribute("style", "inset: 166px auto auto 326px;border: 1px solid rgb(0, 0, 0);height: 440px;margin: 0px;opacity: 1;overflow: auto;padding: 0px;position: fixed;width: 650px;z-index: 9999;display: block;");
                 },
-                'save': function () {
+                "save": function () {
                     GM_config.close();
                     if (changeCallback) {
                         changeCallback();
@@ -92,7 +92,7 @@ var TorrenterConfigurator = /** @class */ (function () {
     Object.defineProperty(TorrenterConfigurator.prototype, "localization", {
         get: function () {
             if (!TorrenterConfigurator._localization.hasOwnProperty(this._language)) {
-                return TorrenterConfigurator._localization['en'];
+                return TorrenterConfigurator._localization["en"];
             }
             return TorrenterConfigurator._localization[this._language];
         },
@@ -110,10 +110,10 @@ var TorrenterConfigurator = /** @class */ (function () {
     };
     TorrenterConfigurator.getLanguage = function () {
         var lang = (window.navigator.languages ? window.navigator.languages[0] : window.navigator.language).toLowerCase();
-        if (lang.indexOf('-') !== -1)
-            lang = lang.split('-')[0];
-        if (lang.indexOf('_') !== -1)
-            lang = lang.split('_')[0];
+        if (lang.indexOf("-") !== -1)
+            lang = lang.split("-")[0];
+        if (lang.indexOf("_") !== -1)
+            lang = lang.split("_")[0];
         return lang;
     };
     TorrenterConfigurator.prototype.getConfiguration = function () {
@@ -129,39 +129,39 @@ var TorrenterConfigurator = /** @class */ (function () {
                 "https://www.torrentdownloads.me/search/?new=1&s_cat=0&search={title} {year}",
                 "https://www.limetorrents.pro/search/all/{title} {year}/seeds/1/"
             ],
-            showEngines: this.getConfigurationProperty('showEngines', true),
-            showUserEngines: this.getConfigurationProperty('showUserEngines', false),
-            showUserEnginesFirst: this.getConfigurationProperty('showUserEnginesFirst', false),
-            userEngines: this.getConfigurationProperty('userEngines', '').split(/\r?\n/).filter(function (item) { return !!item; }),
+            showEngines: this.getConfigurationProperty("showEngines", true),
+            showUserEngines: this.getConfigurationProperty("showUserEngines", false),
+            showUserEnginesFirst: this.getConfigurationProperty("showUserEnginesFirst", false),
+            userEngines: this.getConfigurationProperty("userEngines", "").split(/\r?\n/).filter(function (item) { return !!item; }),
         };
     };
     ;
     TorrenterConfigurator._localization = {
         en: {
-            settingsTitle: 'Torrenter Script Settings',
-            showBuildInEngines: 'Show Build-in Engines',
-            showUserEngines: 'Show User Definied Engines',
-            showUserEnginesFirst: 'Show User Definied Engines First',
-            userEngines: 'User Engines',
-            eg: 'e.g.',
-            addEngineDescription: 'Type by separating with an enter. Available variables are:</br>&emsp;{title} - movie title</br>&emsp;{year} - movie release year</br>&emsp;{imdb} - position ID in www.imdb.com</br>&emsp;[] - optional fragment, removed if the internal tag is not found by the site processor',
-            saveButtonCaption: 'Save',
-            closeCaptionButton: 'Close',
-            resetLinkCaption: 'Reset to defaults',
-            configureMenuItem: 'Configure'
+            settingsTitle: "Torrenter Script Settings",
+            showBuildInEngines: "Show Build-in Engines",
+            showUserEngines: "Show User Definied Engines",
+            showUserEnginesFirst: "Show User Definied Engines First",
+            userEngines: "User Engines",
+            eg: "e.g.",
+            addEngineDescription: "Type by separating with an enter. Available variables are:</br>&emsp;{title} - movie title</br>&emsp;{year} - movie release year</br>&emsp;{imdb} - position ID in www.imdb.com</br>&emsp;[] - optional fragment, removed if the internal tag is not found by the site processor",
+            saveButtonCaption: "Save",
+            closeCaptionButton: "Close",
+            resetLinkCaption: "Reset to defaults",
+            configureMenuItem: "Configure"
         },
         pl: {
-            settingsTitle: 'Ustawienia skryptu Torrenter',
-            showBuildInEngines: 'Pokaż wyszukiwarki wbudowane',
-            showUserEngines: 'Pokaż wyszukiwarki użytkownika',
-            showUserEnginesFirst: 'Pokaż wyszukiwarki użytkownika jako pierwsze',
-            userEngines: 'Wyszukiwarki użytkownika',
-            eg: 'np.',
-            addEngineDescription: 'Podaj rozdzielając enterem. Dostępne zmienne to:</br>&emsp;{title} - tytuł filmu</br>&emsp;{year} - rok wydania filmu</br>&emsp;{imdb} - ID pozycji w serwisie www.imdb.com</br>&emsp;[] - fragment opcjonalny usuwany jeżeli wewnętrzny tag nie zostanie odnaleziony przez parser strony',
-            saveButtonCaption: 'Zapisz',
-            closeCaptionButton: 'Anuluj',
-            resetLinkCaption: 'Przywróć ustawienia domyślne',
-            configureMenuItem: 'Skonfiguruj'
+            settingsTitle: "Ustawienia skryptu Torrenter",
+            showBuildInEngines: "Pokaż wyszukiwarki wbudowane",
+            showUserEngines: "Pokaż wyszukiwarki użytkownika",
+            showUserEnginesFirst: "Pokaż wyszukiwarki użytkownika jako pierwsze",
+            userEngines: "Wyszukiwarki użytkownika",
+            eg: "np.",
+            addEngineDescription: "Podaj rozdzielając enterem. Dostępne zmienne to:</br>&emsp;{title} - tytuł filmu</br>&emsp;{year} - rok wydania filmu</br>&emsp;{imdb} - ID pozycji w serwisie www.imdb.com</br>&emsp;[] - fragment opcjonalny usuwany jeżeli wewnętrzny tag nie zostanie odnaleziony przez parser strony",
+            saveButtonCaption: "Zapisz",
+            closeCaptionButton: "Anuluj",
+            resetLinkCaption: "Przywróć ustawienia domyślne",
+            configureMenuItem: "Skonfiguruj"
         }
     };
     return TorrenterConfigurator;
@@ -171,7 +171,7 @@ var Torrenter = /** @class */ (function () {
     }
     Torrenter.prototype.apply = function (config, siteProcessor) {
         var _this = this;
-        var torrenterElements = document.getElementsByClassName('torrenter');
+        var torrenterElements = document.getElementsByClassName("torrenter");
         if (torrenterElements && torrenterElements.length > 0) {
             for (var i = torrenterElements.length - 1; i >= 0; i--) {
                 torrenterElements[i].remove();
@@ -277,7 +277,7 @@ var Torrenter = /** @class */ (function () {
                 year = match[1];
             }
         }
-        var headerElement = document.querySelector('.fP__titleDetails');
+        var headerElement = document.querySelector(".fP__titleDetails");
         if (headerElement && title) {
             headerElement.appendChild(createLinkSpan("span", "display: inline-flex;", "position: relative; top: 2px; z-index: 1;", { title: title, year: year }));
         }
@@ -328,7 +328,7 @@ var Torrenter = /** @class */ (function () {
                 year = match[1];
             }
         }
-        var headerElement = document.querySelector('.scoreboard__title');
+        var headerElement = document.querySelector(".scoreboard__title");
         if (headerElement && title) {
             headerElement.appendChild(createLinkSpan("span", "margin-left: 1em;font-size: 0.5em;position: relative;top: -7px;", "position: relative; top: 2px;", { title: title, year: year }));
         }
--- torrenter.user.ts
+++ torrenter.user.ts
@@ -40,42 +40,42 @@ declare let GM_registerMenuCommand: Function;
 type CreateLinkSpanFunction = (tag: string, style: string, itemStyle: string, args: Object) => HTMLElement;
 type SiteProcessFunction = (createLinkSpan: CreateLinkSpanFunction) => void;
 
-'use strict';
+"use strict";
 class TorrenterConfigurator {
     private _language: string;
 
     private static readonly _localization: Object = {
         en: {
-            settingsTitle: 'Torrenter Script Settings',
-            showBuildInEngines: 'Show Build-in Engines',
-            showUserEngines: 'Show User Definied Engines',
-            showUserEnginesFirst: 'Show User Definied Engines First',
-            userEngines: 'User Engines',
-            eg: 'e.g.',
-            addEngineDescription: 'Type by separating with an enter. Available variables are:</br>&emsp;{title} - movie title</br>&emsp;{year} - movie release year</br>&emsp;{imdb} - position ID in www.imdb.com</br>&emsp;[] - optional fragment, removed if the internal tag is not found by the site processor',
-            saveButtonCaption: 'Save',
-            closeCaptionButton: 'Close',
-            resetLinkCaption: 'Reset to defaults',
-            configureMenuItem: 'Configure'
+            settingsTitle: "Torrenter Script Settings",
+            showBuildInEngines: "Show Build-in Engines",
+            showUserEngines: "Show User Definied Engines",
+            showUserEnginesFirst: "Show User Definied Engines First",
+            userEngines: "User Engines",
+            eg: "e.g.",
+            addEngineDescription: "Type by separating with an enter. Available variables are:</br>&emsp;{title} - movie title</br>&emsp;{year} - movie release year</br>&emsp;{imdb} - position ID in www.imdb.com</br>&emsp;[] - optional fragment, removed if the internal tag is not found by the site processor",
+            saveButtonCaption: "Save",
+            closeCaptionButton: "Close",
+            resetLinkCaption: "Reset to defaults",
+            configureMenuItem: "Configure"
         },
         pl: {
-            settingsTitle: 'Ustawienia skryptu Torrenter',
-            showBuildInEngines: 'Pokaż wyszukiwarki wbudowane',
-            showUserEngines: 'Pokaż wyszukiwarki użytkownika',
-            showUserEnginesFirst: 'Pokaż wyszukiwarki użytkownika jako pierwsze',
-            userEngines: 'Wyszukiwarki użytkownika',
-            eg: 'np.',
-            addEngineDescription: 'Podaj rozdzielając enterem. Dostępne zmienne to:</br>&emsp;{title} - tytuł filmu</br>&emsp;{year} - rok wydania filmu</br>&emsp;{imdb} - ID pozycji w serwisie www.imdb.com</br>&emsp;[] - fragment opcjonalny usuwany jeżeli wewnętrzny tag nie zostanie odnaleziony przez parser strony',
-            saveButtonCaption: 'Zapisz',
-            closeCaptionButton: 'Anuluj',
-            resetLinkCaption: 'Przywróć ustawienia domyślne',
-            configureMenuItem: 'Skonfiguruj'
+            settingsTitle: "Ustawienia skryptu Torrenter",
+            showBuildInEngines: "Pokaż wyszukiwarki wbudowane",
+            showUserEngines: "Pokaż wyszukiwarki użytkownika",
+            showUserEnginesFirst: "Pokaż wyszukiwarki użytkownika jako pierwsze",
+            userEngines: "Wyszukiwarki użytkownika",
+            eg: "np.",
+            addEngineDescription: "Podaj rozdzielając enterem. Dostępne zmienne to:</br>&emsp;{title} - tytuł filmu</br>&emsp;{year} - rok wydania filmu</br>&emsp;{imdb} - ID pozycji w serwisie www.imdb.com</br>&emsp;[] - fragment opcjonalny usuwany jeżeli wewnętrzny tag nie zostanie odnaleziony przez parser strony",
+            saveButtonCaption: "Zapisz",
+            closeCaptionButton: "Anuluj",
+            resetLinkCaption: "Przywróć ustawienia domyślne",
+            configureMenuItem: "Skonfiguruj"
         }
     }
 
     private get localization(): any {
         if (!TorrenterConfigurator._localization.hasOwnProperty(this._language)) {
-            return TorrenterConfigurator._localization['en'];
+            return TorrenterConfigurator._localization["en"];
         }
 
         return TorrenterConfigurator._localization[this._language];
@@ -92,11 +92,11 @@ class TorrenterConfigurator {
     static getLanguage(): string {
         let lang = (window.navigator.languages ? window.navigator.languages[0] : window.navigator.language).toLowerCase();
 
-        if (lang.indexOf('-') !== -1)
-            lang = lang.split('-')[0];
+        if (lang.indexOf("-") !== -1)
+            lang = lang.split("-")[0];
 
-        if (lang.indexOf('_') !== -1)
-            lang = lang.split('_')[0];
+        if (lang.indexOf("_") !== -1)
+            lang = lang.split("_")[0];
 
         return lang;
     }
@@ -114,10 +114,10 @@ class TorrenterConfigurator {
                 "https://www.torrentdownloads.me/search/?new=1&s_cat=0&search={title} {year}",
                 "https://www.limetorrents.pro/search/all/{title} {year}/seeds/1/"
             ],
-            showEngines: this.getConfigurationProperty('showEngines', true),
-            showUserEngines: this.getConfigurationProperty('showUserEngines', false),
-            showUserEnginesFirst: this.getConfigurationProperty('showUserEnginesFirst', false),
-            userEngines: this.getConfigurationProperty('userEngines', '').split(/\r?\n/).filter((item: any) => { return !!item; }),
+            showEngines: this.getConfigurationProperty("showEngines", true),
+            showUserEngines: this.getConfigurationProperty("showUserEngines", false),
+            showUserEnginesFirst: this.getConfigurationProperty("showUserEnginesFirst", false),
+            userEngines: this.getConfigurationProperty("userEngines", "").split(/\r?\n/).filter((item: any) => { return !!item; }),
         };
     }
 
@@ -129,56 +129,56 @@ class TorrenterConfigurator {
         }
 
         let gmConfiguration = {
-            'id': 'mabakay_Torrenter',
-            'title': this.localization.settingsTitle,
-            'fields': {
-                'showEngines': {
-                    'label': this.localization.showBuildInEngines,
-                    'type': 'checkbox',
-                    'default': true
+            "id": "mabakay_Torrenter",
+            "title": this.localization.settingsTitle,
+            "fields": {
+                "showEngines": {
+                    "label": this.localization.showBuildInEngines,
+                    "type": "checkbox",
+                    "default": true
                 },
-                'showUserEngines': {
-                    'label': this.localization.showUserEngines,
-                    'type': 'checkbox',
-                    'default': false
+                "showUserEngines": {
+                    "label": this.localization.showUserEngines,
+                    "type": "checkbox",
+                    "default": false
                 },
-                'showUserEnginesFirst': {
-                    'label': this.localization.showUserEnginesFirst,
-                    'type': 'checkbox',
-                    'default': false
+                "showUserEnginesFirst": {
+                    "label": this.localization.showUserEnginesFirst,
+                    "type": "checkbox",
+                    "default": false
                 },
-                'userEngines': {
-                    'label': this.localization.userEngines,
-                    'type': 'textarea',
-                    'default': '',
+                "userEngines": {
+                    "label": this.localization.userEngines,
+                    "type": "textarea",
+                    "default": "",
                 }
             },
-            'events': {
-                'open': (document: Document, window: Window, frame: any) => {
-                    let userEnginesFiled = document.getElementById('mabakay_Torrenter_field_userEngines');
-                    userEnginesFiled.setAttribute('cols', '80');
-                    userEnginesFiled.setAttribute('rows', '10');
-                    userEnginesFiled.setAttribute('placeholder', this.localization.eg + ' https://search-site.com/?title={title}&year={year}&orderby=seeds[&imdbTag={imdb}]');
+            "events": {
+                "open": (document: Document, window: Window, frame: any) => {
+                    let userEnginesFiled = document.getElementById("mabakay_Torrenter_field_userEngines");
+                    userEnginesFiled.setAttribute("cols", "80");
+                    userEnginesFiled.setAttribute("rows", "10");
+                    userEnginesFiled.setAttribute("placeholder", this.localization.eg + " https://search-site.com/?title={title}&year={year}&orderby=seeds[&imdbTag={imdb}]");
 
                     let enginesFieldDescription = document.createElement("div");
-                    enginesFieldDescription.setAttribute('style', 'font-size: 12px;margin: 5px 6px;color: gray;');
+                    enginesFieldDescription.setAttribute("style", "font-size: 12px;margin: 5px 6px;color: gray;");
                     enginesFieldDescription.innerHTML = this.localization.addEngineDescription;
 
-                    let enginesLabelField = document.getElementById('mabakay_Torrenter_userEngines_field_label');
+                    let enginesLabelField = document.getElementById("mabakay_Torrenter_userEngines_field_label");
                     enginesLabelField.after(enginesFieldDescription);
 
-                    let saveButton = <Element>document.getElementById('mabakay_Torrenter_saveBtn');
+                    let saveButton = <Element>document.getElementById("mabakay_Torrenter_saveBtn");
                     saveButton.textContent = this.localization.saveButtonCaption;
 
-                    let closeButton = <Element>document.getElementById('mabakay_Torrenter_closeBtn');
+                    let closeButton = <Element>document.getElementById("mabakay_Torrenter_closeBtn");
                     closeButton.textContent = this.localization.closeCaptionButton;
 
-                    let restToDefaultsLink = <Element>document.getElementById('mabakay_Torrenter_resetLink');
+                    let restToDefaultsLink = <Element>document.getElementById("mabakay_Torrenter_resetLink");
                     restToDefaultsLink.textContent = this.localization.resetLinkCaption;
 
-                    GM_config.frame.setAttribute('style', 'inset: 166px auto auto 326px;border: 1px solid rgb(0, 0, 0);height: 440px;margin: 0px;opacity: 1;overflow: auto;padding: 0px;position: fixed;width: 650px;z-index: 9999;display: block;');
+                    GM_config.frame.setAttribute("style", "inset: 166px auto auto 326px;border: 1px solid rgb(0, 0, 0);height: 440px;margin: 0px;opacity: 1;overflow: auto;padding: 0px;position: fixed;width: 650px;z-index: 9999;display: block;");
                 },
-                'save': () => {
+                "save": () => {
                     GM_config.close();
 
                     if (changeCallback) {
@@ -195,7 +195,7 @@ class TorrenterConfigurator {
 
 class Torrenter {
     apply(config: Object, siteProcessor: SiteProcessFunction): void {
-        let torrenterElements = document.getElementsByClassName('torrenter');
+        let torrenterElements = document.getElementsByClassName("torrenter");
         if (torrenterElements && torrenterElements.length > 0) {
             for (let i = torrenterElements.length - 1; i >= 0; i--) {
                 torrenterElements[i].remove();
@@ -326,7 +326,7 @@ class Torrenter {
             }
         }
 
-        let headerElement = document.querySelector('.fP__titleDetails');
+        let headerElement = document.querySelector(".fP__titleDetails");
         if (headerElement && title) {
             headerElement.appendChild(createLinkSpan("span", "display: inline-flex;", "position: relative; top: 2px; z-index: 1;", { title, year }));
         }
@@ -390,7 +390,7 @@ class Torrenter {
             }
         }
 
-        let headerElement = document.querySelector('.scoreboard__title');
+        let headerElement = document.querySelector(".scoreboard__title");
         if (headerElement && title) {
             headerElement.appendChild(createLinkSpan("span", "margin-left: 1em;font-size: 0.5em;position: relative;top: -7px;", "position: relative; top: 2px;", { title, year }));
         }
